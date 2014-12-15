c3f97bcf12859ee20e7a7609d0577aa537a2a45b
liviu- <lp752@york.ac.uk>
2014-12-15 22:45:50 +0000

    Added more functionality.

--- mailinator-f5/mailinator-f5.py
+++ mailinator-f5/mailinator-f5.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python
-import sys
-import json
+#encoding: utf-8
+
+import time
 import argparse
 import requests
-import pprint
 
 from IPython import embed as qq
-
 
 try:
     from apikey import API
 except ImportError:
     API = input('Please provide your API key ')
     
-
-
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Mailinator inbox checker")
     parser.add_argument('inbox', nargs=1, help='The inbox that you want to check')
     return parser.parse_args()
 
-def check_inbox(inbox_page):
+def is_new(emails):
+    return bool(next((email for email in emails if email['seconds_ago'] < 200), False))
+
+def check_inbox(inbox):
+    inbox_page = get_inbox(inbox)
     return inbox_page['messages']
+
+def wait_email(inbox):
+    while (1):
+        emails = check_inbox(inbox)
+        if is_new(emails):
+            print('new email')
+        print('test')
+        time.sleep(5)
 
 def get_inbox(inbox):
     return requests.get('https://api.mailinator.com/api/inbox?to={inbox}&token={api}'.format(
@@ -34,8 +43,7 @@ def start_daemon():
 def main():
     args = parse_arguments() 
     start_daemon()
-    inbox_page = get_inbox(args.inbox[0])
-    emails = check_inbox(inbox_page)
+    wait_email(args.inbox[0])
 
 if __name__ == '__main__':
     main()
